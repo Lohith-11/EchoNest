640358ef662898afb873d17e4b400fb8927afc4f
Lohith-11 <lohithr011@gamil.com>
2025-02-18 21:09:31 +0530

    Server Validation done

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path=require('path');
 const Listing=require("./models/listing");
 const methodOverride=require('method-override');
 const ejsMate=require('ejs-mate');
+const wrapAsync=require("./utils/wrapAsync");
+const ExpressError=require("./utils/ExpressError");
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -32,63 +35,63 @@ app.get("/",(req,res)=>{
 
 
 
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     const allListings= await Listing.find({});
     res.render("./listings/index.ejs",{allListings});
-});
+}));
 
 app.get("/listings/new",(req,res)=>{
     res.render("./listings/new.ejs");
 });
 
 
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     const {id}=req.params;
     const listing= await Listing.findById(id);
     res.render("./listings/show.ejs",{listing});
-});
+}));
 
-app.post("/listings",async (req,res)=>{
+app.post("/listings",wrapAsync(async (req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Listing not sent properly");
+    }
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("./listings/edit.ejs",{listing});
-});
+}));
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Listing not sent properly");
+    }
     let {id}=req.params;
      await Listing.findByIdAndUpdate(id,{...req.body.listing});
      res.redirect(`/listings/${id}`);
-})
+}));
 
 
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing= await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
 })
 
-
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing=new Listing({
-//         title:"My new Villa",
-//         description:"On the mountain",
-//         price:12000,
-//         location:"Hyderabad, Telangana",
-//         country:"India"
-//     });
-//     await sampleListing.save()
-//     console.log("Sample was saved");
-//     res.send("successful Testing");
-// });
-
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something Went Wrong!!"}=err;
+    res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
+})
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
